80ab0dd272ac1df4fe9823da9e91f7782e7896a7
AEPKILL <a@aepkill.com>
2018-02-25 13:50:52 +0800

    fix type error

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,6 @@
 import { Options } from 'node-sass';
+import { Plugin } from 'webpack';
+
 export type NODE_ENV = 'production' | 'development';
 export type FileRule = string | string[] | { [key: string]: string };
 export interface Config {
@@ -6,7 +8,7 @@ export interface Config {
   autoprefixer?: boolean;
   sass?: Options;
 }
-export default class SassPlugin {
+export default class SassPlugin extends Plugin {
   constructor(file: FileRule, mode?: NODE_ENV | Config);
   constructor(file: FileRule, mode: NODE_ENV, config?: Config);
 }
