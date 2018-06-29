30377db65d4f4e16e37e63ae390bc497778ab1a8
Christian Schätzlein <chris@matoki.de>
2018-06-29 13:51:00 +0200

    Add support for webpack 4+
    Fix TypeError if used with webpack 4+

--- src/audit.js
+++ src/audit.js
@@ -1,7 +1,11 @@
 const EXCLUDE_PATTERN = /node_modules|bower_components/;
 
 function addDep(list, item) {
-  if(list.indexOf(item) === -1) list.push(item);
+    if(typeof list.has === 'function') {
+        if(!list.has(item)) list.add(item);
+    } else {
+        if(list.indexOf(item) === -1) list.push(item);
+    }
 }
 
 export default class Audit {
